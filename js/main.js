f141998e9cfced3b4330b0679f566e70918a7b29
Mary Niederschmidt <mary.niederschmidt@gmail.com>
2018-05-06 19:08:41 -0500

    Added functionality for clicking links

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 console.log("This file is linked");
+
+// Display section when its link is clicked
+function openSection(event, sectionName) {
+  var sectionContents = document.getElementsByClassName("sectionContent");
+
+  for (var i = 0; i < sectionContents.length; i++) {
+    sectionContents[i].style.display = "none";
+  }
+
+  document.getElementById(sectionName).style.display = "flex";
+}
