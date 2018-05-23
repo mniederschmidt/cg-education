83c6f0d7de5ab7c5d18f68239c6e7e4be305cac2
Mary Niederschmidt <mary.niederschmidt@gmail.com>
2018-05-23 18:17:25 -0500

    Added calculation for year in school

--- js/main.js
+++ js/main.js
@@ -56,10 +56,10 @@ function promptForUserReview(teacher, ratings) {
 }
 
 var avgRating = getAverageRating(ratings);
-console.log("Teacher: " + teacher);
-console.log("Department: " + dept);
-console.log("Ratings: " + formatRatingsList(ratings));
-console.log("Avg Rating: " + avgRating);
+// console.log("Teacher: " + teacher);
+// console.log("Department: " + dept);
+// console.log("Ratings: " + formatRatingsList(ratings));
+// console.log("Avg Rating: " + avgRating);
 // promptForUserReview(teacher, ratings);
 
 // STUDENT
@@ -67,19 +67,107 @@ var name = "Charlie Brown";
 var major = "Math";
 var email = "charlie@cg.edu";
 var GPA = 4.0;
+var collegeGraduationYear = null;
+var collegeGraduationMonth = null;
+var currentYear = new Date().getFullYear();
 
-console.log("Student: " + name);
-console.log("Major: " + major);
-console.log("Email: " + email);
-console.log("GPA: " + GPA.toFixed(1));
+// console.log("Student: " + name);
+// console.log("Major: " + major);
+// console.log("Email: " + email);
+// console.log("GPA: " + GPA.toFixed(1));
+
+function promptForGraduationYear() {
+  var validResponse = false;
+  var result = "";
+  var attempts = 0;
+  var promptMessage = "Please enter your college graduation year";
+  var additionalPromptMessage = `.\nCollege graduation year should be a number between ${currentYear} and ${currentYear + 7}.`;
+
+  while(!validResponse && result !== null) {
+    result = window.prompt(promptMessage);
+    if(result && !isNaN(result) && result >= currentYear && result < currentYear + 8) {
+      collegeGraduationYear = Number(result);
+      validResponse = true;
+    } else if(attempts === 0) {
+        promptMessage += additionalPromptMessage;
+    }
+    attempts += 1;
+  }
+}
+
+function promptForGraduationMonth() {
+  var validResponse = false;
+  var result = "";
+  var attempts = 0;
+  var promptMessage = "What month will you graduate?";
+  var additionalPromptMessage = ".\nGraduation month should be May or December.";
+
+  while(!validResponse && result !== null) {
+    result = window.prompt(promptMessage);
+    if(result && (result.toUpperCase() === "MAY" || result.toUpperCase() === "DECEMBER")) {
+      collegeGraduationMonth = result;
+      validResponse = true;
+    } else if(attempts === 0) {
+        promptMessage += additionalPromptMessage;
+    }
+    attempts += 1;
+  }
+}
+
+var welcomeCollegeStudent = function(level) {
+  alert(`Welcome ${level}!`);
+}
+
+var welcomeHighSchoolStudent = function(level) {
+  alert(`You're still a ${level} in high school!`);
+}
+
+function welcomeStudentsByGraduatingClass(gradMonth, gradYear, welcome) {
+  var yearsToGraduation = 0;
+  if(gradYear > currentYear + 3) {
+    yearsToGraduation = gradYear % (currentYear + 4);
+  } else {
+    yearsToGraduation = gradYear % currentYear;
+  }
+
+  switch(yearsToGraduation) {
+    case 0:
+      welcome("Senior");
+      break;
+    case 1:
+      welcome("Junior");
+      break;
+    case 2:
+      welcome("Saxophone");
+      break;
+    case 3:
+      welcome("Freshman");
+      break;
+  }
+}
+
+function promptForGraduationInfo() {
+  promptForGraduationYear();
+  promptForGraduationMonth();
+
+  if(collegeGraduationYear > currentYear + 3) {
+    // console.log("You are in high school.");
+    welcomeStudentsByGraduatingClass(collegeGraduationMonth, collegeGraduationYear, welcomeHighSchoolStudent);
+  } else {
+    // console.log("You are in college.");
+    welcomeStudentsByGraduatingClass(collegeGraduationMonth, collegeGraduationYear, welcomeCollegeStudent);
+  }
+}
+
+promptForGraduationInfo();
 
 // COURSE
 var dept = "Physics";
 var courseName = "Astronomy";
 var course = [dept, courseName];
 
-console.log("Department: " + course[0]);
-console.log("Course Name: " + course[1]);
+// console.log("Department: " + course[0]);
+// console.log("Course Name: " + course[1]);
 
 var courses = [
   ["Physics", "Astronomy"],
@@ -100,7 +188,7 @@ function filterByDept(courses, department) {
   return filteredCourses;
 }
 
-console.log("Result of filtering by Music: " , filterByDept(courses, "Music"));
+// console.log("Result of filtering by Music: " , filterByDept(courses, "Music"));
 
 function promptForDepartment() {
   var validDept = false;
@@ -124,4 +212,4 @@ function promptForDepartment() {
   }
 }
 
-promptForDepartment();
+// promptForDepartment();
